eec5c0e247353a0d409eff52e8a4b7bd889b3ea6
Nikita <39399744+NikitaElmanov@users.noreply.github.com>
2023-04-30 11:33:08 +0300

    additional

---
+++ DgsGraphqlDemo/src/main/java/dgs/graphql/demo/DgsGraphqlDemoApplication.java
@@ -0,0 +1,13 @@
+package dgs.graphql.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsGraphqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgsGraphqlDemoApplication.class, args);
+	}
+
+}
---
+++ DgsGraphqlDemo/src/test/java/dgs/graphql/demo/DgsGraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dgs.graphql.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgsGraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
