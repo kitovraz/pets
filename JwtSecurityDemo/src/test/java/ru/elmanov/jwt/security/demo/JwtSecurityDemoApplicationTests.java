1982c18ea65c7761b0652a6f03e7a3a609460b80
Nikita <39399744+NikitaElmanov@users.noreply.github.com>
2023-05-12 10:34:05 +0300

    init jwt project commit

---
+++ JwtSecurityDemo/src/test/java/ru/elmanov/jwt/security/demo/JwtSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.elmanov.jwt.security.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
