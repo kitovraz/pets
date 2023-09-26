22a5aeaac4d5ea5f063010738ffa3bf3c111dbcf
Nikita <39399744+NikitaElmanov@users.noreply.github.com>
2023-09-26 09:16:18 +0300

    init commmit

---
+++ avro-kafka-test/src/test/java/ru/elmanov/avrokafkatest/AvroKafkaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.elmanov.avrokafkatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvroKafkaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
