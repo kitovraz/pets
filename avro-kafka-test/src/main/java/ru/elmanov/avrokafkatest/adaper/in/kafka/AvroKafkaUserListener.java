268961467f3ca690b4ec68265fbd35f16f026c42
Nikita <39399744+NikitaElmanov@users.noreply.github.com>
2023-10-12 16:50:07 +0300

    add different avro schemas entities separation

--- avro-kafka-test/src/main/java/ru/elmanov/avrokafkatest/adaper/in/kafka/AvroKafkaUserListener.java
+++ avro-kafka-test/src/main/java/ru/elmanov/avrokafkatest/adaper/in/kafka/AvroKafkaUserListener.java
@@ -5,6 +5,8 @@ import org.apache.avro.specific.SpecificRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import ru.elmanov.avrokafkatest.api.rq.avro.SecondEntityAvro;
+import ru.elmanov.avrokafkatest.api.rq.avro.UserAvro;
 
 @Slf4j
 @Service
@@ -13,6 +15,14 @@ public class AvroKafkaUserListener {
     @KafkaListener(topics = "${avro.topic.name}",
             containerFactory = "kafkaListenerContainerFactoryUser")
     public void listen(ConsumerRecord<String, SpecificRecord> record) {
+        final var value = record.value();
+        if (value instanceof UserAvro) {
+            log.info("[UserAvro]");
+        }
+        else if (value instanceof SecondEntityAvro) {
+            log.info("[SecondEntityAvro]");
+        }
+
         log.info("Message successfully received.\n key: {}, value: {}, offset: {}, timestamp: {}, timestampType: {}, serializedKeySize: {}",
                 record.key(),
                 record.value(),
