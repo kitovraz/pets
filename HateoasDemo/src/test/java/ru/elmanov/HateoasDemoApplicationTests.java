80163551e0a5b82d21d0355058b5d3f1c415bc62
Nikita <39399744+NikitaElmanov@users.noreply.github.com>
2024-02-08 10:49:02 +0300

    init

---
+++ HateoasDemo/src/test/java/ru/elmanov/HateoasDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.elmanov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
